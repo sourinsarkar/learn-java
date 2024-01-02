6de6db53d8a7d358de4bd38500c030fcdbf506fd
Sourin <iamsourinsarkar@gmail.com>
2024-01-02 10:17:30 +0530

    Optimised file structure

---
+++ basics/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Started with Java.");
+        
+        float b = 20;
+        System.out.println(b);
+    }
+}
