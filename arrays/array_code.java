b045ae3d93e7c7c3d148011fad9592d771e30db2
Sourin <iamsourinsarkar@gmail.com>
2024-01-03 23:50:48 +0530

    Practiced Arrays is Java

---
+++ arrays/array_code.java
@@ -0,0 +1,23 @@
+class array_code {
+    public static void main(String[] args) {
+        // int[] some = new int[5];
+        // some[0] = 10;
+        // some[1] = 20;
+        // some[2] = 30;
+        // some[3] = 40;
+        // some[4] = 50;
+
+        // System.out.println(some[0]); 
+
+        // int count = 0;
+        // for(int thing: some) {
+        //     System.out.println("Index: " + count);
+        //     System.out.println("Value: " + thing);
+        //     count++;
+        // }
+
+        // Multi-dimensional Arrays -----------------
+
+        int[][ multi = new int[3][4];
+    }
+}
