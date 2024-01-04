75490f176c1cdfb2ea063ed91e2a7f7db15a3d87
Sourin <iamsourinsarkar@gmail.com>
2024-01-04 12:47:43 +0530

    Learned copying of arrays in Java

--- arrays/array_code.java
+++ arrays/array_code.java
@@ -1,3 +1,5 @@
+// import java.util.Arrays;
+
 class array_code {
     public static void main(String[] args) {
         // int[] some = new int[5];
@@ -18,6 +20,50 @@ class array_code {
 
         // Multi-dimensional Arrays -----------------
 
-        int[][ multi = new int[3][4];
+        // int[][] multi = new int[3][4];
+
+        // Copy of an array -------------------------
+
+        // int[] numbers = {100,200,300,400,500};
+        // int[] new_numbers = numbers;
+        // int[] new1 = new int[numbers.length];
+
+        // for(int items: numbers) {
+        //     System.out.println(items);
+        // }
+
+        // System.out.println();
+        
+        // for(int items: new_numbers) {
+        //     System.out.println(items);
+        // }
+        
+        // System.out.println();
+
+        // int i = 0;
+        // for(int items: numbers) {
+        //     new1[i] = items;
+        //     i++;
+        // }
+
+        // for(int items: new1) {
+        //     System.out.println(items);
+        // }
+
+        // --------------------------------------
+
+        int[] n1 = {2, 3, 12, 4, 12, -2};
+        int[] n2 = new int[n1.length];
+        // System.out.println(n1.length);
+        // System.out.println(n2.length);
+        // System.out.println(n2[2]);
+        // n2[2] = 23;
+        // System.out.println(n2[2]);
+
+        System.arraycopy(n1, 0, n2, 0, n1.length);
+
+        for (int j : n2) {
+            System.out.print(j);
+        }   
     }
 }
