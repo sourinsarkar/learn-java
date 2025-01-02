6fe401aff7eb4c5ce2be9aa188fc7b4845d7bd65
Sourin <iamsourinsarkar@gmail.com>
2025-01-02 18:29:22 +0530

    Pushing previous updates

--- Code.java
+++ Code.java
@@ -2,11 +2,33 @@ class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
 }
+
+class Computer {
+    public void playMusic()
+    {
+        System.out.println("Music playing...");
+    }
+
+    public String getMePen(int cost)
+    {
+        if(cost >= 10)
+            return "Pen";
+        
+        return "Nothing";
+    }
+}
+
+
 
 public class Code {
     public static void main(String[] a) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.add(2, 3));
+        Calculator obj = new Calculator();
+        System.out.println("1st: " + obj.add(1, 2, 3));
+        System.out.println("2nd: " + obj.add(1, 2));
     }
 }
